e69d22cb2daf2a7839defa0f5226d2de8935fe47
avival69 <aswinkrishnatu@gmail.com>
2026-01-07 19:58:55 +0530

    ds

--- heed-app/App.js
+++ heed-app/App.js
@@ -1,14 +1,12 @@
-import "./global.css"; // <--- Add this import!
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import "./global.css"
+import { Text, View } from "react-native";
+ 
 export default function App() {
   return (
-    <View className="flex-1 bg-gray-200 items-center justify-center">
-      <Text className="text-blue-500 text-2xl font-bold">
-        Hello Tailwind!
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
       </Text>
-      <StatusBar style="auto" />
     </View>
-  );
-}
+  ); 
+} 
